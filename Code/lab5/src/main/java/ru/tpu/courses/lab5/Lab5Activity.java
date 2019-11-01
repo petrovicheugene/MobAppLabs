010d5877a917bdeb86d05d3888d65c8419da9c1a
Eugene Petrovich <petrovich.eugene@gmail.com>
2019-11-01 11:44:04 +0600

    Lab2 Start

---
+++ Code/lab5/src/main/java/ru/tpu/courses/lab5/Lab5Activity.java
@@ -0,0 +1,22 @@
+package ru.tpu.courses.lab5;
+
+import android.content.Intent;
+import androidx.annotation.NonNull;
+import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lab5Activity extends AppCompatActivity {
+
+    public static Intent newIntent(@NonNull Context context) {
+        return new Intent(context, Lab5Activity.class);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab5);
+        setTitle(R.string.lab5_app_name);
+    }
+}
